a75599f4df38bbac7be2b464061ab54f24157b69
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-11 13:54:26 -0400

    tests pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
-# Add seed data in this file
+puts "seed some spells wooooo"
+
+spells = ["acid-arrow", "animal-messenger", "calm-emotions", "charm-person"]
+
+spells.each do |spell|
+    response = RestClient.get  "https://www.dnd5eapi.co/api/spells/#{spell}"
+    spell_book = JSON.parse(response)
+    Spell.create(
+        name: spell_book["name"],
+        level: spell_book["level"],
+        description: spell_book["desc"][0]
+    )
+end
+
+puts "you're welcome"
